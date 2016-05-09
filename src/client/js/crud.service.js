b4dbeb8a4839f099601566301d774ebea6014ffc
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 12:57:34 -0600

    Added crud service globally.

---
+++ src/client/js/crud.service.js
@@ -0,0 +1,61 @@
+(function() {
+  angular.module('myApp')
+    .service('crudService', ['$http', function($http) {
+      var baseUrl = 'http://localhost:3000/api';
+      return {
+        getAll: function(resource) {
+          console.log('resource', resource);
+          return $http({
+            method: 'GET',
+            url: baseUrl + '/' + resource
+          }).then(function(data) {
+            console.log('Crud Service Data', data);
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+        getOne: function(resource, id) {
+          return $http({
+            method: 'GET',
+            url: baseUrl + '/'
+          }).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+        insertOne: function(resource, data) {
+          return $http({
+            method: 'POST',
+            url: baseUrl + '/' + resource,
+            data: data,
+          }).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+        updateOne: function(resource, id, data) {
+          return $http({
+            method: 'PUT',
+            url: baseUrl + '/' + resource + '/' + id
+          }).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+        deleteOne: function(resource, id) {
+          return $http({
+            method: 'DELETE',
+            url: baseUrl + '/' + resource + '/' + id
+          }).then(function(data) {
+            return data;
+          }).catch(function(err) {
+            return err;
+          });
+        },
+      };
+    }]);
+})();
