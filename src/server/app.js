a4684eda9c62acccffe98e63ca3d8e6c738bda24
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 15:52:27 -0600

    Added auth routes.'

--- src/server/app.js
+++ src/server/app.js
@@ -11,6 +11,7 @@ var swig = require('swig');
 // *** routes *** //
 var routes = require('./routes/index.js');
 var apiRoutes = require('./routes/api.js');
+var authRoutes = require('./routes/auth.js');
 
 
 // *** express instance *** //
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 // *** main routes *** //
 app.use('/', routes);
 app.use('/api', apiRoutes);
+app.use('/auth', authRoutes);
 
 
 // catch 404 and forward to error handler
