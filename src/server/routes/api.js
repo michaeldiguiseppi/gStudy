021998ab60dee988bd75031e4a25820e403f9635
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 16:38:11 -0600

    Added post routes for cards and decks.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -14,6 +14,10 @@ function Cards () {
 router.get('/decks', getDecks);
 router.get('/decks/:id', getOneDeck);
 router.get('/cards/:id', getCards);
+router.post('/decks/:id', postDecks);
+router.post('/cards/:id', postCards);
+
+////////////////////////////////
 
 function getDecks(req, res) {
   Decks().then(function(data) {
@@ -29,6 +33,22 @@ function getOneDeck(req, res) {
 
 function getCards(req, res) {
   Cards().where('deck_id', req.params.id).then(function(data) {
+    res.json(data);
+  });
+}
+
+function postDecks(req, res) {
+  console.log('req.body', req.body);
+  req.body.user_id = req.params.id;
+  delete req.body.cards;
+  Decks().insert(req.body).returning('id').then(function(data) {
+    res.json(data);
+  });
+}
+
+function postCards(req, res) {
+  req.body.deck_id = req.params.id;
+  Cards().insert(req.body).returning('*').then(function(data) {
     res.json(data);
   });
 }
