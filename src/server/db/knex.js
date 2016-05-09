978b0c987d242aa0b371ab16b26605d1cd150d61
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 12:56:48 -0600

    Fixed issue where the knexfile was not reading the right directory.

--- src/server/db/knex.js
+++ src/server/db/knex.js
@@ -1,3 +1,3 @@
 var environment = process.env.NODE_ENV || 'development';
-var config = require('../../knexfile.js')[environment];
+var config = require('../../../knexfile.js')[environment];
 module.exports = require('knex')(config);
