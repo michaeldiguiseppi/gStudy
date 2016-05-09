ccf92bef8377d3d5cf3536797c192277748a6df8
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 16:37:35 -0600

    Added links and checks for user in order to allow deck creation.

--- src/client/js/components/home/home.controller.js
+++ src/client/js/components/home/home.controller.js
@@ -1,7 +1,13 @@
 angular.module('myApp')
-  .controller('HomeCtrl', ['$scope', 'HomeService', function($scope, HomeService) {
+  .controller('HomeCtrl', ['$scope', '$rootScope', 'HomeService', function($scope, $rootScope, HomeService) {
     $scope.message = 'Hello World! How ya doing?';
     HomeService.getAll().then(function(data) {
-      $scope.decks = data;
+      $scope.decks = data.filter(function(deck) {
+        if ($rootScope.currentUser) {
+          return deck.user_id === JSON.parse($rootScope.currentUser).id;
+        } else {
+          return deck;
+        }
+      });
     });
   }]);
