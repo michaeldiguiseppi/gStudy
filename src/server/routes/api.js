6cc94d25835847a567d879c0d6a3fa92b49e65d3
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 13:41:50 -0600

    Added get one deck API route.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -6,10 +6,16 @@ function Decks () {
 }
 
 router.get('/decks', getDecks);
+router.get('/decks/:id', getOneDeck);
 
 function getDecks(req, res) {
   Decks().then(function(data) {
-    console.log('API data', data);
+    res.json(data);
+  });
+}
+
+function getOneDeck(req, res) {
+  Decks().where('id', req.params.id).then(function(data) {
     res.json(data);
   });
 }
