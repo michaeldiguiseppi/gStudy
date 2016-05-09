95161a0fff54932112701c5756581b3d46a49832
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-09 15:52:56 -0600

    Added card route.

--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -4,9 +4,16 @@ var knex = require('../db/knex.js');
 function Decks () {
   return knex('decks');
 }
+function Users () {
+  return knex('users');
+}
+function Cards () {
+  return knex('cards');
+}
 
 router.get('/decks', getDecks);
 router.get('/decks/:id', getOneDeck);
+router.get('/cards/:id', getCards);
 
 function getDecks(req, res) {
   Decks().then(function(data) {
@@ -16,6 +23,12 @@ function getDecks(req, res) {
 
 function getOneDeck(req, res) {
   Decks().where('id', req.params.id).then(function(data) {
+    res.json(data);
+  });
+}
+
+function getCards(req, res) {
+  Cards().where('deck_id', req.params.id).then(function(data) {
     res.json(data);
   });
 }
