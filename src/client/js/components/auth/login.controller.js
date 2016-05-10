9f2722c640419d559a733e2f7e12b33cd7f74a3b
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-10 16:10:50 -0600

    Added auth to compare passwords.

--- src/client/js/components/auth/login.controller.js
+++ src/client/js/components/auth/login.controller.js
@@ -2,17 +2,19 @@ angular.module('myApp')
   .controller('AuthCtrl', ['$scope', '$rootScope', 'AuthService', '$state',
   function($scope, $rootScope, AuthService, $state) {
       $scope.user = {};
+      $scope.errors = [];
       $scope.login = function() {
         AuthService.login(this.user).then(function(data) {
           if (data.status === 200) {
             AuthService.setUserInfo(data);
             $state.go('home');
           } else {
-            $scope.error = "Email or Password is incorrect.  Please try again.";
+            $scope.errors.push("Email or Password is incorrect.  Please try again.");
           }
         })
         .catch(function(err) {
           console.log(err);
+          $scope.errors.push(err);
         });
       };
   }]);
