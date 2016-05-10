be8406fdee3ae72c530f697aa2a4c8e696b2641c
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-10 10:43:11 -0600

    Added crud actions to pull names of user and deck.

--- src/client/js/components/sockets/sockets.controller.js
+++ src/client/js/components/sockets/sockets.controller.js
@@ -1,12 +1,29 @@
 (function() {
   angular.module('myApp')
-    .controller('SocketCtrl', ['SocketService', '$scope', function(SocketService, $scope) {
-      $scope.notifications = [];
+    .controller('SocketCtrl', ['SocketService', '$scope', '$rootScope', 'crudService', function(SocketService, $scope, $rootScope, crudService) {
+      $rootScope.notifications = $rootScope.notifications || [];
       console.log('Controller!');
       SocketService.forward('deck.studying', $scope);
-          $scope.$on('socket:deck.studying', function (ev, data) {
-            console.log('from angular!', data);
-            $scope.notifications.push(data);
+      $scope.$on('socket:deck.studying', function (ev, data) {
+        console.log('from angular!', data);
+        crudService.getOne('user', data.user_id).then(function(user) {
+          crudService.getOne('decks', data.deck_id).then(function(deck) {
+            data.user = user.data[0];
+            data.deck = deck.data[0].deck_name;
+            $rootScope.notifications.push(data);
           });
+        });
+      });
+      SocketService.forward('deck.created', $scope);
+      $scope.$on('socket:deck.created', function (ev, data) {
+        console.log('from angular!', data);
+        crudService.getOne('user', data.user_id).then(function(user) {
+          crudService.getOne('decks', data.deck_id).then(function(deck) {
+            data.user = user.data[0];
+            data.deck = deck.data[0].deck_name;
+            $rootScope.notifications.push(data);
+          });
+        });
+      });
     }]);
 })();
