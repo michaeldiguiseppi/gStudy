0700a1009dc672e3c11bd8bb1edcfbf750c9712e
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-05-10 10:43:56 -0600

    Added event listener for socket when studying a deck.

--- src/client/js/components/study/study.controller.js
+++ src/client/js/components/study/study.controller.js
@@ -1,15 +1,16 @@
 (function() {
 
   angular.module('myApp')
-    .controller('StudyCtrl', ['$scope', '$stateParams', 'StudyService', 'SocketService',
-      function($scope, $stateParams, StudyService, SocketService) {
+    .controller('StudyCtrl', ['$scope', '$stateParams', '$rootScope', 'StudyService', 'SocketService',
+      function($scope, $stateParams, $rootScope, StudyService, SocketService) {
         $scope.showAnswer = false;
         StudyService.getDeck($stateParams.id).then(function(deck) {
           $scope.deck = deck;
           StudyService.getCards(deck.id).then(function(cards) {
+            var user_id = JSON.parse($rootScope.currentUser).id;
             $scope.cards = cards;
             $scope.remaining = cards.length;
-            SocketService.emit('deck.studying', { message: 'Studying Deck', id: 1 });
+            SocketService.emit('deck.studying', { message: 'Studying Deck', deck_id: deck.id, user_id: user_id });
           });
         });
         $scope.cardNum = 0;
